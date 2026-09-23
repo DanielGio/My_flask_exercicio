e53b1dfd2a08a8303c9731e9025537585a32c94e
Daniel Giovane Florencio <florencedgio@gmail.com>
2026-09-23 20:02:15 -0300

    20260923_2001
    estrutura básica criada

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Meu primeiro Flask</h1>"
+    return render_template("index.html")
+
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contacts", methods=["GET", "POST"])
+def contacts():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+
+        print("Nome:", name)
+        print("E-mail:", email)
+        print("Assunto:", subject)
+        print("Mensagem:", message)
+
+    return render_template("contacts.html")
 
 
 if __name__ == "__main__":
